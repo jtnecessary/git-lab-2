307a4398c5ff2a82156a5d4ca9eafa6f2fca4ddc
jtnecessary <112732386+jtnecessary@users.noreply.github.com>
2022-09-09 23:26:52 -0400

    Add files via upload

---
+++ answers.md.cpp
@@ -0,0 +1,30 @@
+answers.md
+// Answer 1-- git version is 2.37.2.windows.2
+/*Answer 2--diff.astextplain.textconv=astextplain
+filter.lfs.clean=git-lfs clean -- %f
+filter.lfs.smudge=git-lfs smudge -- %f
+filter.lfs.process=git-lfs filter-process
+filter.lfs.required=true
+http.sslbackend=openssl
+http.sslcainfo=C:/Program Files/Git/mingw64/ssl/certs/ca-bundle.crtcore.autocrlf=true
+core.fscache=true
+core.symlinks=false
+pull.rebase=false
+credential.helper=manager-core
+/*
+// Answer 3-- When I typed git add --help it took me to a page on how to use the comman, as if it was a hyperlink 
+//Answer 4-- When I typed git status it came up as having no commits, but I had two untracekd files. README.md and answers.md 
+//Answer 5-- After adding README.md it became a green line compared to red before
+//Answer 6-- Both were green and waiting to be committed
+//Answer 7-- After typing git status after committing the files, it says nothing to commit, working tree clean
+/*Answer 8--commit 9810bbdbfa4f28af25676af7645d7a60dea64934 (HEAD -> master)
+Author: jtnecessary <jn089521@ohio.edu>
+Date:   Tue Sep 6 17:26:41 2022 -0400
+
+    Initial commit
+    /*
+//Answer 9 It says my branch is up to date with origin/main
+//Answer 10 No the changes were not made in my local copy
+//Answer 11 It tells me I should use git pull before then trying to push because there are changes not made locally
+//Answer 12 The changes made from online were there after the pull
+//Answer 13 It shows me the files in the repository
